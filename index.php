3675149e72d2934f2ada6fde99c20c85c396140c
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-11-06 19:54:03 +0200

    ok

--- index.php
+++ index.php
@@ -1,56 +1,57 @@
- <?php require_once("./settings.php")?>
+<?php require_once("./settings.php") ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $name?></title>
+    <title><?php echo $name ?></title>
 
     <style>
-        body{
-            background-color: <?=$settings["background"]?>;
-            font-family: <?=$settings["font"]?>;
-            font-size: <?=$settings["fontSize"]?>;
+        body {
+            background-color:
+                <?= $settings["background"] ?>
+            ;
+            font-family:
+                <?= $settings["font"] ?>
+            ;
+            font-size:
+                <?= $settings["fontSize"] ?>
+            ;
         }
-
     </style>
 </head>
-<body>
-     <?php if($settings["WebSiteEnabled"]):?>
 
-        <?php //include_once("./nav.php")?>
-        <?php require_once("./nav.php")?>
-       
-        <?php if(!isset($_GET["sekcija"]))
-            {
-                header("Location:?sekcija=1",true);
-            }    
+<body>
+    <?php if ($settings["WebSiteEnabled"]): ?>
+
+        <?php //include_once("./nav.php") ?>
+        <?php require_once("./nav.php") ?>
+
+        <?php if (!isset($_GET["sekcija"])) {
+            header("Location:?sekcija=1", true);
+        }
         ?>
 
-        <?php if($_GET["sekcija"]==1):?> 
-       
-            
-        <h1>Sekcijas 1 saturs</h1>
-        <p>Sekcijas 1 saturs</p>
-
-        <?php elseif($_GET["sekcija"]==2) :?>    
+        <?php if ($_GET["sekcija"] == 1): ?>
+            <h1>Sekcijas 1 saturs</h1>
+            <p>Sekcijas 1 saturs</p>
+        <?php elseif ($_GET["sekcija"] == 2): ?>
             <h1>Sekcijas 2 saturs</h1>
             <p>Sekcijas 2 saturs</p>
-        <?php else :?>
-            
+        <?php else: ?>
             <h1>Lapa nav atrasta</h1>
+        <?php endif; ?>
 
-        <?php endif;?>    
+        <script>
 
-
-    <script>
-
-    </script>
-    <?php else:?>
+        </script>
+    <?php else: ?>
         <h1>Mājaslapa ir slēgta</h1>
-    <?php endif;?>    
+    <?php endif; ?>
 
 </body>
+
 </html>
