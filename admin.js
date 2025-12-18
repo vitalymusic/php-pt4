9742245a1d426842c2bb6d46e0fa24fe284c8f87
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-12-18 20:04:58 +0200

    bildes izdēšana

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
-$(document).ready(()=>{
-    $('#file_upload').on('submit', (e)=>{
+$(document).ready(() => {
+    $('#file_upload').on('submit', (e) => {
         e.preventDefault();
 
         let form = $('#file_upload')[0];
@@ -7,51 +7,56 @@ $(document).ready(()=>{
 
         $.ajax({
             url: 'functions.php?action=uploadfile',
-            contentType: false, 
+            contentType: false,
             type: 'POST',
             data: data,
             processData: false,
-            success: (data)=> {
-                    data = JSON.parse(data);
-                 if(data.upload==="success"){
+            success: (data) => {
+                data = JSON.parse(data);
+                if (data.upload === "success") {
                     form.reset();
-                 }else{
+                } else {
                     alert(JSON.parse(data).error);
-                 }
+                }
             }
         })
 
     })
 
-
+    loadImages();
     // Bilžu parādīšana admin panelī
 
     // {"id":"1","image_name":"bilde","url":"images.jpeg"}
-
-    $.get('./functions.php?action=listImages',(data)=>{
-        data = JSON.parse(data);
-        data.forEach((item,i)=>{
-            $('.photos').append(`
+    function loadImages() {
+        $('.photos').empty();
+        $.get('./functions.php?action=listImages', (data) => {
+            data = JSON.parse(data);
+            data.forEach((item, i) => {
+                $('.photos').append(`
                 <div class="image">
                     <img src="./upload/${item.url}">
                     <p>${item.image_name}</p>
                     <button data-id="${item.id}" data-filename="${item.url}">Izdzēst bildi</button>    
             `)
-        })
-    }).then(()=>{
-                $('.photos button').click((e)=>{
-                    id = e.target.dataset.id;
-                    fileName = e.target.dataset.filename;
-                    if(confirm("Tiešām izdzēst?",false)){
-                        $.post('./functions.php?action=deleteImage&id=' + id,{
-                            fileName:fileName
-                        })
-                    }
-                });
-
             })
+        }).then(() => {
+            $('.photos button').click((e) => {
+                id = e.target.dataset.id;
+                fileName = e.target.dataset.filename;
+                if (confirm("Tiešām izdzēst?", false)) {
+                    $.post('./functions.php?action=deleteImage&id=' + id, {
+                        fileName: fileName
+                    })
+                    .then(()=>{loadImages()});
+                }
+            });
+
+        })
+    }
+
 
 })
+
 
 
 // $_SERVER['REQUEST_METHOD'] === 'PUT'
